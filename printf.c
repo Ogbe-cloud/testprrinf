eb2d258fb2f761eb89e532c4993a5bf9dc0f7111
Ogbe-cloud <104437711+Ogbe-cloud@users.noreply.github.com>
2023-10-17 12:33:39 -0700

    Update printf.c

--- printf.c
+++ printf.c
@@ -4,7 +4,7 @@ int _printf(const char *format, ...) {
     char buffer[BUFF_SIZE];
     int bufferIndex = 0;
     int count = 0;
-    int i;  // Define 'i' as an integer variable
+    int i;  
 
     if (format == NULL)
         return -1;
@@ -14,48 +14,44 @@ int _printf(const char *format, ...) {
 
     for (i = 0; format[i] != '\0'; i++) {
         if (format[i] != '%') {
-            // Append character to buffer
             buffer[bufferIndex] = format[i];
             bufferIndex++;
         } else {
             switch (format[i + 1]) {
                 case 'c':
-                    // Call function to handle character
-                    character(args, &count);  // Assuming you have this function defined
+                   
+                    character(args, &count);  
                     break;
                 case 's':
                     // Call function to handle string
-                    _string(args, &count);  // Assuming you have this function defined
+                    _string(args, &count); 
                     break;
                 case '%':
-                    // Call function to handle percent
-                    percent(&count);  // Assuming you have this function defined
+                    
+                    percent(&count);  
                     break;
                 case 'b':
-                    // Call function to handle binary
-                    binary(args, &count);  // Assuming you have this function defined
+                    
+                    binary(args, &count);  
                     break;
                 case 'd':
                 case 'i':
-                    // Call function to handle integer
-                    _integer(args, &count);  // Assuming you have this function defined
+                    
+                    _integer(args, &count);  
                     break;
                 default:
-                    // Write the character directly
+                    
                     write(1, &format[i], 1);
                     count++;
             }
             i++;
         }
-
-        // Check buffer size and flush if needed
         if (bufferIndex == BUFF_SIZE) {
             write(1, buffer, bufferIndex);
-            bufferIndex = 0;  // Reset bufferIndex after flushing
+            bufferIndex = 0;  
         }
     }
 
-    // Flush remaining characters in buffer
     if (bufferIndex > 0) {
         write(1, buffer, bufferIndex);
     }
